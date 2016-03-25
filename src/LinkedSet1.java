982a005a59b763b3b78b0f76e86015a1738802bb
Chigozie <chygoz2@gmail.com>
2016-03-25 07:12:58 +0000

    1A done

--- src/LinkedSet1.java
+++ src/LinkedSet1.java
@@ -72,13 +72,11 @@ public class LinkedSet1<E extends Comparable<E>> implements Set<E> {
 
 		while(curr != null && it.hasNext()){ // while both this set and that set have elements
 			E thatCurr = it.next(); //get the next element in that set
-			int comp = thatCurr.compareTo(curr.elem); //check if that's element comes before or after the element in curr
 
-			while (comp > 0 && curr.next != null){ //while thatCurr comes after curr. Keep checking subsequent elements in this for a match
+			while (thatCurr.compareTo(curr.elem) > 0 && curr.next != null){ //while thatCurr comes after curr. Keep checking subsequent elements in this for a match
 				curr = curr.next;
-				comp = thatCurr.compareTo(curr.elem);
 			}
-			if (comp == 0){ // if a match is found, get the node in this. the loop to check if both sets still have elements will be repeated
+			if (thatCurr.equals(curr.elem)){ // if a match is found, get the node in this. the loop to check if both sets still have elements will be repeated
 				curr = curr.next;
 			}
 			else { // if a match was not found. Then this set does not contain all of that. Terminate
@@ -135,10 +133,14 @@ public class LinkedSet1<E extends Comparable<E>> implements Set<E> {
 			return;
 		}
 		//set is not empty, look for node containing it
-		Node<E> curr = first, pred = null;
+		Node<E> curr = first, pred = first;
 		while(curr != null){
 			if(curr.elem.equals(it)){ //if element is found remove it
-				pred = curr.next;
+				if(curr.equals(pred)){ //element found at first position
+					first = curr.next;
+				}else{
+					pred.next = curr.next;
+				}
 				size--;
 				return;
 			}
@@ -155,9 +157,49 @@ public class LinkedSet1<E extends Comparable<E>> implements Set<E> {
 	// where n' is size of merged SLL
 	public void addAll(Set<E> that) {
 		// TODO Auto-generated method stub
-		Iterator<E> it = that.iterator();
+
+		Iterator<E> it = that.iterator(); //get iterator on second set
+		Node<E> curr = first, prev = first;
 		
-			
+		while (it.hasNext()){ //while second set still has elements
+			E thatCurr = it.next(); //get next element of second set
+			if(curr != null ){  //if first set still has elements
+				while(thatCurr.compareTo(curr.elem) > 0){ //compare the element in the current node with the element of the second set.
+												//while the second set's element is greater than that of the first, check the next element
+												//of the first set
+					prev = curr;
+					curr = curr.next;
+				}
+				
+				if(thatCurr.equals(curr.elem)){ //if both elements of the two sets are equal, then get their next elements
+					prev = curr;
+					curr = curr.next;
+					thatCurr = it.next();
+				}
+				if(thatCurr.compareTo(curr.elem) < 0){ //if the second set's element is smaller, insert it before the current element of first set
+					Node<E> ins = new Node<E>(thatCurr,curr);
+					if(prev.equals(curr)){ // if the current element in first set is the first element, point first to it
+						first = ins;
+						prev = first;
+					}
+					else{ //otherwise insert the second set's element before curr
+						prev.next = ins;
+						prev = ins;
+					}
+					size++;
+				}
+				
+			}else{ //first set has no other elements. Insert remaining elements of second set into first set
+				Node<E> ins = new Node<E>(thatCurr, null);
+				if(first == null){
+					first = ins;
+					prev = first;
+				}else{
+					prev.next = ins;
+					prev = ins;
+				}
+			}
+		}	
 	}
 
 	@Override
@@ -173,18 +215,16 @@ public class LinkedSet1<E extends Comparable<E>> implements Set<E> {
 		Iterator<E> it = that.iterator();
 		while(it.hasNext() && curr != null){ //while both sets have elements
 			E thatCurr = it.next();
-			int comp = thatCurr.compareTo(curr.elem); //check if that's element comes before or after this's element
-			while(comp > 0 && curr.next != null){ // if it comes after, get next element in this and check again
+			while(thatCurr.compareTo(curr.elem) > 0 && curr.next != null){ // if it comes after, get next element in this and check again
 				prev = curr;
 				curr = curr.next;
-				comp = thatCurr.compareTo(curr.elem);
 			}
-			if(comp == 0){ // if that's element is found in this set, 
+			if(thatCurr.equals(curr.elem)){ // if that's element is found in this set, 
 				if(prev.equals(curr)){ //if it was found at the first position, remove the first element
 					first = curr.next;
 					curr = first;
 					prev = curr;
-				}else{ //it is not the first element to be removed
+				}else{ //it is not the first element to be removed. Remove from it's position
 					prev.next = curr.next;
 					curr = curr.next;
 				}
@@ -201,6 +241,37 @@ public class LinkedSet1<E extends Comparable<E>> implements Set<E> {
 	public void retainAll(Set<E> that) {
 		// TODO Auto-generated method stub
 		
+		if(first == null){ //if this is empty, don't bother checking. return
+			return;
+		}
+		int tempSize = 0;
+		Node<E> curr = first, prev = first;
+		Iterator<E> it = that.iterator();
+		if(!it.hasNext()){
+			clear();
+			return;
+		}
+		while(it.hasNext() && curr != null){ //while both sets have elements
+			E thatCurr = it.next();
+			while(thatCurr.compareTo(curr.elem) > 0 && curr.next != null){ // 
+				if(prev.equals(curr)){
+					first = curr.next;
+					prev = curr = first;
+				}else{
+					prev.next = curr.next;
+					curr = curr.next;
+				}
+			}
+			if(thatCurr.equals(curr.elem)){ // if that's element is found in this set, 
+				prev = curr;
+				curr = curr.next;
+				tempSize++;
+			}
+		}
+		if(curr != null){ //if that runs out of elements while this still has elements, remove the remaining elements of this
+			prev.next = null;
+		}
+		size = tempSize;
 	}
 
 	@Override
@@ -258,5 +329,17 @@ public class LinkedSet1<E extends Comparable<E>> implements Set<E> {
 		}
 		System.out.print("}");
 		System.out.println();
+	}
+	
+	//tostring method. to be removed
+	public String toString(){
+		Node<E> curr = first;
+		String s = "{";
+		while(curr != null){
+			s+=(curr.elem + ", ");
+			curr = curr.next;
+		}
+		s+="}";
+		return s;
 	}
 }
--- src/LinkedSet1Test.java
+++ src/LinkedSet1Test.java
@@ -5,38 +5,44 @@ import org.junit.Test;
 
 public class LinkedSet1Test {
 	
-	private static LinkedSet1<Integer> set;
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		set = new LinkedSet1<Integer>();
-		set.add(10);
-	}
 
 	@Test
 	public void testIsEmpty() {
 		//fail("Not yet implemented");
+		LinkedSet1<Integer> set = new LinkedSet1<Integer>();
+		set.add(10);
 		assertFalse(set.isEmpty());
 	}
 	
 	@Test
 	public void testSize() {
 		//fail("Not yet implemented");
+		LinkedSet1<Integer> set = new LinkedSet1<Integer>();
+		set.add(20);set.add(10);set.add(20);
 		assertEquals("Test size",2,set.size());
 	}
 
 	@Test
 	public void testContains() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		LinkedSet1<Integer> set = new LinkedSet1<Integer>();
+		set.add(20);set.add(10);set.add(30);
+		assertTrue("Test contains", set.contains(20));
+		assertFalse("Test contains", set.contains(40));
 	}
 
 	@Test
 	public void testEqualsSetOfE() {
 		//fail("Not yet implemented");
+		LinkedSet1<Integer> setz = new LinkedSet1<Integer>();
 		LinkedSet1<Integer> that = new LinkedSet1<Integer>();
+		setz.add(10); setz.add(12);setz.add(20);setz.add(30);
 		that.add(10);
 		that.add(20);
-		assertTrue("Test if two sets are equal", set.equals(that));
+		assertFalse("Test if two sets are equal", setz.equals(that));
+		setz.remove(12);setz.remove(30);
+		//System.out.println(setz);
+		assertTrue("Test if two sets are equal after", setz.equals(that));
 	}
 
 	@Test
@@ -48,30 +54,42 @@ public class LinkedSet1Test {
 		set11.add(3); set11.add(4);
 		set11.add(5); set11.add(6);
 		
-		set21.add(3); set21.add(5); set21.add(6); set21.add(1);
+		set21.add(3); set21.add(5); set21.add(6); 
 		
 		assertTrue("Test if set1 contains set2", set11.containsAll(set21));
 	}
 
 	@Test
 	public void testClear() {
-		fail("Not yet implemented");
-		//set.clear();
-		//assertEquals("Test size after clearing", 0, set.size());
+		//fail("Not yet implemented");
+		LinkedSet1<Integer> set = new LinkedSet1<Integer>();
+		set.add(10); set.add(20);
+		set.clear();
+		assertEquals("Test size after clearing", 0, set.size());
+		assertEquals("Content after clearing", "{}", set.toString());
 	}
 
 	@Test
 	public void testAdd() {
 		//fail("Not yet implemented");
+		LinkedSet1<Integer> set = new LinkedSet1<Integer>();
 		set.add(20);
-		assertEquals("Test size after adding one element",2,set.size());
+		assertEquals("Test size after adding one element",1,set.size());
+		assertEquals("Content after adding one element", "{20, }", set.toString());
+		set.add(5);
+		assertEquals("Content after adding two elements", "{5, 20, }", set.toString());
 	}
 
 	@Test
 	public void testRemove() {
-		fail("Not yet implemented");
-		//set.remove(10);
-		//assertEquals("Test size after removal of one element",1,set.size());
+		//fail("Not yet implemented");
+		LinkedSet1<Integer> set = new LinkedSet1<Integer>();
+		set.add(10); set.add(20);
+		assertEquals("Test size before removal of one element",2,set.size());
+		set.remove(10); 
+		//System.out.println(set);
+		assertEquals("Test size after removal of one element",1,set.size());
+		assertFalse("test if set still contains removed element", set.contains(10));
 	}
 
 	@Test
@@ -80,7 +98,7 @@ public class LinkedSet1Test {
 		LinkedSet1<Integer> set2 = new LinkedSet1<Integer>();
 		set1.add(3);
 		set1.add(11);
-		set1.add(17);
+		set1.add(17); set1.add(13);
 		set2.add(10);
 		set2.add(13);
 		set2.add(14);
@@ -89,21 +107,35 @@ public class LinkedSet1Test {
 		set1.addAll(set2);
 		//set1.print();
 		assertEquals("Test size of set 1 after union", 7, set1.size());
+		assertEquals("Test contents of set 1 after union", "{3, 10, 11, 13, 14, 16, 17, }", set1.toString());
 	}
 
 	@Test
 	public void testRemoveAll() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		LinkedSet1<Integer> seta = new LinkedSet1<Integer>();
+		LinkedSet1<Integer> setb = new LinkedSet1<Integer>();
+		seta.add(1);seta.add(2);seta.add(3);seta.add(5);seta.add(6);seta.add(9);seta.add(11);
+		setb.add(1); setb.add(5);setb.add(2);setb.add(7);setb.add(9);setb.add(11);
+		seta.removeAll(setb);
+		assertEquals("Testing remove all before", "{3, 6, }", seta.toString());
+		setb.add(3); seta.add(13);
+		seta.removeAll(setb);
+		assertEquals("Testing remove all after", "{6, 13, }", seta.toString());
+		assertEquals("Size of a ", 2, seta.size());
 	}
 
 	@Test
 	public void testRetainAll() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIterator() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		LinkedSet1<Integer> seta = new LinkedSet1<Integer>();
+		LinkedSet1<Integer> setb = new LinkedSet1<Integer>();
+		seta.add(1);seta.add(2);seta.add(3);seta.add(5);seta.add(6);seta.add(9);seta.add(11);seta.add(12);seta.add(13);seta.add(14);
+		setb.add(1); setb.add(5);setb.add(2);setb.add(7);setb.add(9);setb.add(11);
+		seta.retainAll(setb);
+		//System.out.println(seta);
+		assertEquals("Testing retain all", "{1, 2, 5, 9, 11, }", seta.toString());
+		assertEquals("Size of a ", 5, seta.size());
 	}
 
 }
